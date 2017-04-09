630dac8ec93a5c89c4f6bb05ae11e30812a7018e
deciemphysics <deciem.physics@gmail.com>
2017-04-08 21:37:49 -0600

    Lots of stuff. Finalizing

--- js/ajax.js
+++ js/ajax.js
@@ -16,21 +16,34 @@ var lng2 = -111.905075311661;
 var place = {};
 var placeName;
 var placeInfo = {};
-
+var getCoords = {};
+navigator.geolocation.getCurrentPosition(function (location) {
+    getCoords.latitude = location.coords.latitude;
+    getCoords.longitude = location.coords.longitude;
+});
+var numPlayers = 0;
 // Adding and initializing google maps API
 function initMap() {
-    var loc1 = {lat: lat1, lng: lng1};
-    var loc2 = {lat: lat2, lng: lng2};
+    var loc1 = {
+        lat: lat1,
+        lng: lng1
+    };
+    var loc2 = {
+        lat: lat2,
+        lng: lng2
+    };
     //var loc1 = {lat: 40.431459252532, lng: -111.905075311661};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
+        zoom: 14,
         center: loc1,
         mapTypeID: 'satellite'
     });
     var marker1 = new google.maps.Marker({
         position: loc1,
         map: map,
-        icon: {url: 'resources/images/golf-ball.png'}
+        icon: {
+            url: 'resources/images/golf-ball.png'
+        }
 
     });
     var marker2 = new google.maps.Marker({
@@ -44,6 +57,7 @@ function initMap() {
 function getCourses() {
     return new Promise(function (resolve) {
         var xhttp = new XMLHttpRequest();
+        var body;
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200) {
                 var data = JSON.parse(xhttp.responseText);
@@ -54,17 +68,25 @@ function getCourses() {
         };
         xhttp.open("POST", "https://golf-courses-api.herokuapp.com/courses/", true);
         xhttp.setRequestHeader('Content-Type', 'application/json');
-        var body = {
-            "latitude": 40.431459252532,
-            "longitude": -111.905075311661,
-            "radius": 10
-        };
+        if (getCoords.latitude == null) {
+            body = {
+                "latitude": 40.431459252532,
+                "longitude": -111.905075311661,
+                "radius": 30
+            };
+        } else {
+            body = {
+                "latitude": getCoords.latitude,
+                "longitude": getCoords.longitude,
+                "radius": 30
+            }
+        }
         xhttp.send(JSON.stringify(body));
     });
 }
 
 
-function getCourseData(id){
+function getCourseData(id) {
     return new Promise(function (resolve) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
@@ -82,50 +104,136 @@ function getCourseData(id){
         xhttp.send();
     });
 }
-// function getPlace(placeName){
-//     return new Promise(function (resolve,reject){
-//         var xhttp = new XMLHttpRequest;
-//         xhttp.onreadystatechange = function(){
-//             if(xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200){
-//                 place = JSON.parse(xhttp.responseText);
-//             }
-//         };
-//         xhttp.open("GET", "https://maps.googleapis.com/maps/api/place/textsearch/json?" + placeName + "&AIzaSyAmiXBLuqOYrtXAdM_e5SBm9WfHBABKb-0" , true);
-//         xhttp.send();
-//     })
-// } 
 
-// function getPlaceDetails(id){
-//     return new Promise(function(resolve,reject){
-//         var xhttp = new XMLHttpRequest();
-//         xhttp.onreadystatechange = function(){
-//             if(xhttp.readyState == XMLHttpRequest.DONE && xhttp.status == 200){
-//                 placeInfo = JSON.parse(xhttp.responseText);
-//                 resolve(placeInfo);
-//             }else{
-//                 reject('err');
-//             }
-//         };
-//         xhttp.open("GET", "https://maps.googleapis.com/maps/api/place/details/json?AIzaSyAmiXBLuqOYrtXAdM_e5SBm9WfHBABKb-0&"+id, true);
-//         xhttp.send();
-//     })
-// }
-// function encodeName(nameOfCourse){
-//     placeName = encodeURIComponent(nameOfCourse);
-// }
+function placeTeeInfo() {
+    document.getElementById('teeOff').innerHTML = "<option>--Please Select--</option>";
+    var len = courseData.course.tee_types.length;
+    for (var i = 0; i < len; i++)
+        document.getElementById('teeOff').innerHTML += '<option value="' + i + '">' + courseData.course.tee_types[i].tee_type + '</option>';
 
-getCourses().then(function() {
-    courses.forEach(function (course) { 
-        document.getElementById('courseSelect').innerHTML += '<option value="' + course.id + '">' + course.name + '</option>'
-    });
-});
 
-function runCourse(id){
-    
- 
-    getCourseData(id).then(function(courseData){console.log(courseData.course.name)});
-    
 }
 
+function placeHoleInfo() {
+    document.getElementById('lengthSelect').innerHTML = '<option>--Please Select--</option>';
+    if (courseData.course.hole_count == 18) {
+        document.getElementById('lengthSelect').innerHTML += '<option value="front9">Front 9</option>';
+        document.getElementById('lengthSelect').innerHTML += '<option value="back9">Back 9</option>';
+        document.getElementById('lengthSelect').innerHTML += '<option value="playAll">Full 18</option>';
+    } else if (courseData.course.hole_count == 9) {
+        document.getElementById('lengthSelect').innerHTML += '<option value="front9">All 9</option>'
+        placeTeeInfo();
+    }
+}
 
-// Alright. So I have a way to calculate total, calculate par, 
+function appendCourseList() {
+    document.getElementById('courseSelect').innerHTML = '<option>--Please Select--</option>'
+    courses.forEach(function (course) {
+        document.getElementById('courseSelect').innerHTML += '<option value="' + course.id + '">' + course.name + '</option>'
+    });
+    document.getElementById('geoButton').innerHTML = '<button class="btn btn-success" type="submit" onclick="updateGeo()">Use Current Loc</button>';
+
+}
+
+function updateGeo() {
+    getCourses().then(appendCourseList);
+    document.getElementById('lengthSelect').innerHTML = '<option>--Please Select--</option>';
+    document.getElementById('teeOff').innerHTML = "<option>--Please Select--</option>";
+    document.getElementById('playerEntry').innerHTML = '<div class="col-xs-12 playerContainer" id="playerContainer"><h3>Please select a course, how many holes you would like to play, and your preferred tee.</h3></div>';
+    document.getElementById('playerEntry2').innerHTML = '';
+    document.getElementById('courseEntry').innerHTML = '';
+    document.getElementById('courseEntry2').innerHTML = '';
+    document.getElementById('playerButton').innerHTML = '';
+}
+
+getCourses().then(appendCourseList);
+
+function runCourse(id) {
+    getCourseData(id).then(function (courseData) {
+        console.log(courseData.course.name)
+    });
+    getCourseData(id).then(placeHoleInfo).then(placeTeeInfo);
+}
+
+function holeMap(holeNum) {
+    var teeId = document.getElementById('teeOff').value;
+    lat1 = courseData.course.holes[holeNum].green_location.lat;
+    lng1 = courseData.course.holes[holeNum].green_location.lng;
+    lat2 = courseData.course.holes[holeNum].tee_boxes[teeId].location.lat;
+    lng2 = courseData.course.holes[holeNum].tee_boxes[teeId].location.lng;
+    initMap();
+
+}
+
+function resetPage() {
+    location.reload();
+}
+// Alright. So I have a way to calculate total, calculate par, SEE SCORE-SERVICE
+//Gotta build my infocard
+function initPlayers(numHoles) {
+    numPlayers = 0;
+    var teeId = document.getElementById('teeOff').value;
+    document.getElementById('courseEntry').innerHTML = '<div class="col-xs-2"><div class="courseNum">Hole</div><div class="yardage">Yardage</div><div class="par">Par</div></div>';
+    document.getElementById('playerContainer').innerHTML = '';
+
+    if (numHoles == 'front9') {
+        document.getElementById('playerButton').innerHTML = '<div class="col-xs-2"><button class="btn btn-success" type="submit" onclick="addPlayer(0)">Add Player</button></div><div class="col-xs-1"><button class="btn btn-danger" type="submit" onclick="resetPage()">Reset</button></div>'
+        for (var i = 0; i < 9; i++) {
+            document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="text-center"><button class="courseNum btn btn-default text-center" type="submit" onclick="holeMap(' + i + ')">' + (i + 1) + '</Button></div><div class="yardage text-center">' + courseData.course.holes[i].tee_boxes[teeId].yards + '</div><div class="par text-center" >' + courseData.course.holes[i].tee_boxes[teeId].par + '</div></div>';
+        }
+        document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="text-center courseNum">Total</div><div class="text-center yardage">' + courseData.course.tee_types[teeId].front_nine_yards + '</div><div class="par text-center" id="par">' + courseData.course.tee_types[teeId].front_nine_par + '</div></div>';
+    } else if (numHoles == 'back9') {
+        document.getElementById('playerButton').innerHTML = '<div class="col-xs-2"><button class="btn btn-success" type="submit" onclick="addPlayer(1)">Add Player</button></div><div class="col-xs-1"><button class="btn btn-danger" type="submit" onclick="resetPage()">Reset</button></div>'
+        for (var i = 9; i < 18; i++) {
+            document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="text-center"><button class="courseNum btn btn-default text-center" type="submit" onclick="holeMap(' + i + ')">' + (i + 1) + '</Button></div><div class="yardage text-center">' + courseData.course.holes[i].tee_boxes[teeId].yards + '</div><div class="par text-center">' + courseData.course.holes[i].tee_boxes[teeId].par + '</div></div>';
+        }
+        document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="courseNum text-center">Total</div><div class="yardage text-center">' + courseData.course.tee_types[teeId].back_nine_yards + '</div><div class="par" id="par">' + courseData.course.tee_types[teeId].back_nine_par + '</div></div>';
+
+    } else {
+        document.getElementById('playerButton').innerHTML = '<div class="col-xs-2"><button class="btn btn-success" type="submit" onclick="addPlayer(2)">Add Player</button></div><div class="col-xs-1"><button class="btn btn-danger" type="submit" onclick="resetPage()">Reset</button></div>'
+        document.getElementById('courseEntry2').innerHTML = '<div class="col-xs-2"><div class="courseNum">Hole</div><div class="yardage">Yardage</div><div class="par">Par</div></div>';
+
+        for (var i = 0; i < 9; i++) {
+            document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="text-center"><button class="courseNum btn btn-default text-center" type="submit" onclick="holeMap(' + i + ')">' + (i + 1) + '</Button></div><div class="yardage text-center">' + courseData.course.holes[i].tee_boxes[teeId].yards + '</div><div class="par text-center" >' + courseData.course.holes[i].tee_boxes[teeId].par + '</div></div>';
+        }
+        document.getElementById('courseEntry').innerHTML += '<div class="col-xs-1"><div class="courseNum">Total</div><div class="yardage">' + courseData.course.tee_types[teeId].front_nine_yards + '</div><div class="par" id="par">' + courseData.course.tee_types[teeId].front_nine_par + '</div></div>';
+        for (var i = 9; i < 18; i++) {
+            document.getElementById('courseEntry2').innerHTML += '<div class="col-xs-1"><div class="text-center"><button class="courseNum btn btn-default text-center" type="submit" onclick="holeMap(' + i + ')">' + (i + 1) + '</Button></div><div class="yardage text-center">' + courseData.course.holes[i].tee_boxes[teeId].yards + '</div><div class="par text-center">' + courseData.course.holes[i].tee_boxes[teeId].par + '</div></div>';
+        }
+        document.getElementById('courseEntry2').innerHTML += '<div class="col-xs-1"><div class="courseNum text-center">Total</div><div class="yardage text-center">' + courseData.course.tee_types[teeId].back_nine_yards + '</div><div class="par text-center" id="par2">' + courseData.course.tee_types[teeId].back_nine_par + '</div></div>';
+
+    }
+}
+
+function addPlayer(holeId) {
+    numPlayers++
+    if (numPlayers <= 4) {
+        document.getElementById('playerContainer').innerHTML += '<div class="row" id="player' + numPlayers + '"></div>';
+        var teeId = document.getElementById('teeOff').value;
+        document.getElementById('player' + numPlayers).innerHTML = '<div class="col-xs-2"><input class="form-control" type="text" placeholder="Name"  id="playerName' + numPlayers + '"></div>';
+        if (holeId == 0) {
+            for (var i = 0; i < 9; i++) {
+                document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1"><input type="number" id="player' + numPlayers + 'hole' + (i + 1) + '" class="form-control"></div>';
+            }
+            document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1" id="player' + numPlayers + 'total"></div>';
+        } else if (holeId == 1) {
+            for (var i = 9; i < 18; i++) {
+                document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1"><input type="number" id="player' + numPlayers + 'hole' + (i + 1) + '" class="form-control"></div>';
+            }
+            document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1" id="player' + numPlayers + 'total"></div>';
+        } else {
+            document.getElementById('playerEntry2').innerHTML += '<div class="col-xs-12" id="playerContainer2"><div class="row" id="player' + numPlayers + '-2"></div></div>'
+            document.getElementById('player' + numPlayers + '-2').innerHTML += '<div class="col-xs-2"></div>';
+            for (var i = 0; i < 9; i++) {
+                document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1"><input type="number" id="player' + numPlayers + 'hole' + (i + 1) + '" class="form-control"></div>';
+            }
+            document.getElementById('player' + numPlayers).innerHTML += '<div Class="col-xs-1" id="player' + numPlayers + 'total"></div>';
+            for (var i = 9; i < 18; i++) {
+                document.getElementById('player' + numPlayers + '-2').innerHTML += '<div Class="col-xs-1"><input type="number" id="player' + numPlayers + 'hole' + (i + 1) + '" class="form-control"></div>';
+            }
+            document.getElementById('player' + numPlayers + '-2').innerHTML += '<div Class="col-xs-1" id="player' + numPlayers + 'total"></div>';
+
+
+        }
+    }
+}
--- spec/LinkedList.spec.js
+++ spec/LinkedList.spec.js
@@ -9,7 +9,6 @@ describe ('LinkedList', function(){
         it('should add a new item to and empty instance and have that be the first',function(){ // Yeah this isnt a good test
             var ron = {name:'Ron'};
             linkedList.insert(ron);
-<<<<<<< HEAD
             expect(linkedList.head).toBe(ron);
         })
         it('should be able to alphabetize stuff',function(){ // these test suck okay
@@ -30,7 +29,6 @@ describe ('LinkedList', function(){
             linkedList.insert(kyle);
             likedList.remove(24);
             expect(linkedList.head).toBe(ron);
-=======
             expect(linkedList.find('Ron')).toBe(ron);
         })
         it('should add an item in the correct spot',function(){
@@ -84,26 +82,16 @@ describe ('LinkedList', function(){
             linkedList.remove(56);
             expect(linkedList.find('Zach')).toBe(null);
             expect(linkedList.find('Ron')).toBe(ron);
-
->>>>>>> 9e99413fc990270c8dcad9be268d3eb3a748d1af
         })
     })
     describe ('find',function(){
         it('should find an item',function(){
-            var ron = {name:'Ron',id:42};
-<<<<<<< HEAD
-            var kyle = {name:'Kyle',id:24};
-            linkedList.insert(ron);
-            linkedList.insert(kyle);
-            expect(linkedList.find('Kyle')).toBe(kyle);
-=======
             var kyle = {name:'Kyle',id:13};
             var zach = {name:'Zach',id:56};
             linkedList.insert(ron);
             linkedList.insert(kyle);
             linkedList.insert(zach);
             expect(linkedList.find('Ron')).toBe(ron);
->>>>>>> 9e99413fc990270c8dcad9be268d3eb3a748d1af
         })
     })
 })
